ec072ab5c50d694cdfd96af4b79b0a866f4c7bf7
kwame Adams <kwameadams77@gmail.com>
2023-09-14 13:09:34 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,18 @@ import './index.css'
 import { BrowserRouter, RouterProvider } from "react-router-dom"
 import { AppRouter } from "./app-router.jsx"
 
+
+const user= {
+	name:'kwame',
+	age:24,
+	food:"friedrice",
+	phone:'iphone'
+
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+		<MyfamilyNameContext.Provider value={user}>
 		<RouterProvider router={AppRouter} />
+		</MyfamilyNameContext.Provider>
   </React.StrictMode>,
 )
